abb2ce132a5ae5f4e67aa7a42217cb193cb29317
Branislav Škumanič <b.skumanic@gmail.com>
2021-06-21 11:35:02 +0200

    update

--- Certifications-Backend-Service-certification-app-h2-web-sec-complete/src/main/resources/data.sql
+++ Certifications-Backend-Service-certification-app-h2-web-sec-complete/src/main/resources/data.sql
@@ -1,15 +1,15 @@
-insert into Certification(id,name,url, price) values (10000,'My certification','cert@ibm.com',200.50);
+insert into certification(id,name,url, price) values (10000,'My certification','cert@ibm.com',200.50);
 insert into certification(id,name,url, price, createdBy, createdDate, lastModifiedBy, lastModifiedDate, version) values (10001,'Java 11','https://oracle.com', 200.50, 'Thats me', sysdate, 'Thats me', sysdate, 0);
 insert into certification(id,name,url, price, version) values (h2seq.nextval,'Spring','https://spring.io', 100.50,0);
 insert into certification(id,name,url, price, version) values (10003,'Spring Boot','https://spring.io', null,0);
 insert into certification(id,name,url, price, version) values (10004,'SAP Admin','https://spa.com', null,0);
 insert into certification(id,name,url, price, version) values (10005,'Oracle 12c Admin','https://oracle.com', 200,0);
 
-insert into user(id,name,surname,email) values(10011,'Elon','Musk','emusk@tesla.com');
-insert into user(id,name,surname,email) values(10012,'Tom','Paul','tpaul@tesla.com');
-insert into user(id,name,surname,email) values(10013,'Tom','Black','tblack@tesla.com');
-insert into user(id,name,surname,email) values(10014,'Zac','Klop','zklop@tesla.com');
-insert into user(id,name,surname,email) values(10015,'Tom','Rodgers','trodgers@tesla.com');
+insert into user(id,name,surname,email,isAdmin,isManager) values(10011,'Elon','Musk','emusk@tesla.com',true,false);
+insert into user(id,name,surname,email,isAdmin,isManager) values(10012,'Tom','Paul','tpaul@tesla.com',false,false);
+insert into user(id,name,surname,email,isAdmin,isManager) values(10013,'Tom','Black','tblack@tesla.com',false,false);
+insert into user(id,name,surname,email,isAdmin,isManager) values(10014,'Zac','Klop','zklop@tesla.com',false,false);
+insert into user(id,name,surname,email,isAdmin,isManager) values(10015,'Tom','Rodgers','trodgers@tesla.com',false,false);
 
 insert into voucher(id,state,vouchercode,validUntil,certification_id) values (10020,'NEW','VC1234',sysdate,10001);
 
