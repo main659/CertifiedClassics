cd4672c1f6b6e39d4aa21b50b92203761337c661
Branislav Škumanič <b.skumanic@gmail.com>
2021-06-22 16:05:22 +0200

    adding new assign vouchers to user funcionality

--- Certifications-Backend-Service-certification-app-h2-web-sec-complete/src/main/java/ibm/java/academy/cerfiticationsapp/controllers/MainRestController.java
+++ Certifications-Backend-Service-certification-app-h2-web-sec-complete/src/main/java/ibm/java/academy/cerfiticationsapp/controllers/MainRestController.java
@@ -3,22 +3,34 @@ package ibm.java.academy.cerfiticationsapp.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import ibm.java.academy.cerfiticationsapp.model.User;
+import ibm.java.academy.cerfiticationsapp.model.Voucher;
 import ibm.java.academy.cerfiticationsapp.repository.UserJpaRepository;
+import ibm.java.academy.cerfiticationsapp.request.VoucherUpdateRequest;
+import ibm.java.academy.cerfiticationsapp.response.MessageResponse;
+import ibm.java.academy.cerfiticationsapp.service.VoucherService;
 
-@Controller
+@RestController
 public class MainRestController {
-    
+    @Autowired
+    private VoucherService voucherService;
+
     @Autowired 
     UserJpaRepository userRepo;
 
@@ -57,4 +69,14 @@ public class MainRestController {
         userRepo.deleteAllById(Arrays.asList(id));
     }
     
+    @RequestMapping(path = "/vouchers/update/" , method = RequestMethod.POST)
+	public ResponseEntity<?> udpateVoucher(@Valid @RequestBody VoucherUpdateRequest updateRequest) {
+		Voucher voucher = voucherService.assignVoucherToUser(updateRequest.getVoucherId(), updateRequest.getVoucherId());
+		if(voucher == null){
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Something is wrong!"));
+		}
+		return ResponseEntity.ok(new MessageResponse("User updated successfully!"));
+	}
 }
--- Certifications-Backend-Service-certification-app-h2-web-sec-complete/src/main/resources/data.sql
+++ Certifications-Backend-Service-certification-app-h2-web-sec-complete/src/main/resources/data.sql
@@ -1,26 +1,29 @@
-insert into certification(id,name,url, price) values (10000,'My certification','cert@ibm.com',200.50);
-insert into certification(id,name,url, price, createdBy, createdDate, lastModifiedBy, lastModifiedDate, version) values (10001,'Java 11','https://oracle.com', 200.50, 'Thats me', sysdate, 'Thats me', sysdate, 0);
-insert into certification(id,name,url, price, version) values (h2seq.nextval,'Spring','https://spring.io', 100.50,0);
+insert into Certification(id,name,url,price) values (10000,'My certification','cert@ibm.com',200.50);
+insert into certification(id,name,url, price, createdBy, createdDate, lastModifiedBy, lastModifiedDate, version) 
+values (10001,'Java 11','https://oracle.com', 200.50, 'Thats me', sysdate, 'Thats me', sysdate, 0);
+
 insert into certification(id,name,url, price, version) values (10003,'Spring Boot','https://spring.io', null,0);
 insert into certification(id,name,url, price, version) values (10004,'SAP Admin','https://spa.com', null,0);
 insert into certification(id,name,url, price, version) values (10005,'Oracle 12c Admin','https://oracle.com', 200,0);
 
-insert into user(id,name,surname,email,isAdmin,isManager) values(10011,'Elon','Musk','emusk@tesla.com',true,false);
-insert into user(id,name,surname,email,isAdmin,isManager) values(10012,'Tom','Paul','tpaul@tesla.com',false,false);
-insert into user(id,name,surname,email,isAdmin,isManager) values(10013,'Tom','Black','tblack@tesla.com',false,false);
-insert into user(id,name,surname,email,isAdmin,isManager) values(10014,'Zac','Klop','zklop@tesla.com',false,false);
-insert into user(id,name,surname,email,isAdmin,isManager) values(10015,'Tom','Rodgers','trodgers@tesla.com',false,false);
+insert into user(id,name,surname,email) values(10011,'Elon','Musk','emusk@tesla.com');
+insert into user(id,name,surname,email) values(10012,'Tom','Paul','tpaul@tesla.com');
+insert into user(id,name,surname,email) values(10013,'Tom','Black','tblack@tesla.com');
+insert into user(id,name,surname,email) values(10014,'Zac','Klop','zklop@tesla.com');
+insert into user(id,name,surname,email) values(10015,'Tom','Rodgers','trodgers@tesla.com');
 
 insert into voucher(id,state,vouchercode,validUntil,certification_id) values (10020,'NEW','VC1234',sysdate,10001);
+
+insert into certification(id,name,url, price, version) values (h2seq.nextval,'Spring','https://spring.io', 100.50,0);
 
 insert into skill(id,name,desc) values (20001, 'Java 11 developer','Java 11 developer');
 insert into skill(id,name,desc) values (20002, 'Java test specialist','Java test specialist');
 insert into skill(id,name,desc) values (20003, 'SAP administration','SAP administration');
 insert into skill(id,name,desc) values (20004, 'Oracle database 12c administrator','Oracle database 12c administrator');
 
+
 insert into certification_skill(certification_id, skill_id) values (10003,20001);
 insert into certification_skill(certification_id, skill_id) values (10003,20002);
 
-insert into certification_skill(certification_id, skill_id) values (10001,20002);
-
 update voucher set user_id=10012 where id = 10020;
+insert into certification_skill(certification_id, skill_id) values (10001,20002);
